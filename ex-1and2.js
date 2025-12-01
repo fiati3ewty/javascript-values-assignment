6871afc8019b6273364144b9c5c03bbe22296df1
fiati3ewty <fiati3ewty@gmail.com>
2025-12-01 20:57:29 +0700

    fix: send awanser my Assingment

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,15 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let surName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const nameRestaurant = "Chef's Table by Chef James";
+const billTotal = 15000;
+const totalRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+billTotal = 5000;
+totalRestaurant = 2500;
